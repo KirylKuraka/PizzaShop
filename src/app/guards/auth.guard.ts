d804dcc241767d8f85ebe33220218ac875ca3207
Kirill Kurako <kiryl.kuraka@innowise-group.com>
2021-08-16 10:39:27 +0300

    fixed product creation

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,8 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Token } from '@angular/compiler/src/ml_parser/lexer';
+import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router } from '@angular/router';
 import { IDENTITY_API_URL } from '../app-injection-tokens';
 import { ACCESS_TOKEN_KEY, AuthService, REFRESH_TOKEN_KEY } from '../services/auth.service';
 
@@ -15,11 +13,13 @@ export class AuthGuard implements CanActivate {
 
   async canActivate(): Promise<boolean> {
     if (!this.as.isAuthenticated()) {
+      localStorage.setItem("isAdmin", "false")
       this.router.navigate(['']);
     }
   
     const isRefrechSuccess = await this.tryRefreshingTokens();
     if (!isRefrechSuccess) {
+      localStorage.setItem("isAdmin", "false")
       this.router.navigate(['login']);
     }
 
@@ -37,7 +37,6 @@ export class AuthGuard implements CanActivate {
     try {
       const response = await this.as.refreshToken(token, refreshToken);
 
-      console.log(response)
       const newToken = (<any>response).body.token;
       const newRefreshToken = (<any>response).body.refreshToken;
 
